7277b0e48753dd7f31bfc665b55caf6dfd8f9f3c
marckszz <mateocab100@gmail.com>
2025-10-21 17:57:34 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/LandingPage-GigMap-V1/',   
+  base: '/LandingPage-GigMap-V1/',         
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)), 
+    },
+  },
 })
